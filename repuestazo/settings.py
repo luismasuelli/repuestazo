995ffc5e415466f3d9e915fc1b33911e12ed5536
luismasuelli <luismasuelli@hotmail.com>
2015-03-06 00:02:31 -0500

    aplicacion de contactos instalada

--- repuestazo/settings.py
+++ repuestazo/settings.py
@@ -34,9 +34,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'stock',
-    # 'advertisement',
-    # 'contact',
+    'customers',
     'pages',
 )
 
