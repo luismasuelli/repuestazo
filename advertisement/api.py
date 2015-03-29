99d1eaaad3971d078aa03df124fe909bf1e467ac
luismasuelli <luismasuelli@hotmail.com>
2015-03-29 17:02:46 -0500

    urls de text-set, banner aleatorio, y text-set aleatorio

--- advertisement/api.py
+++ advertisement/api.py
@@ -1,12 +1,28 @@
 from rest_framework.generics import RetrieveAPIView
-from .serializers import ReelSerializer, BannerSerializer
-from .models import Reel
+from .serializers import ReelSerializer, BannerSerializer, RandomBannerSerializer, RandomTextSetSerializer
+from .models import Reel, TextSet, Banner, RandomBanner, RandomTextSet
 
 
 class BannerView(RetrieveAPIView):
+    queryset = Banner.objects.all()
     serializer_class = BannerSerializer
 
 
 class ReelView(RetrieveAPIView):
     queryset = Reel.objects.ready()
     serializer_class = ReelSerializer
+
+
+class TextSetView(RetrieveAPIView):
+    queryset = TextSet.objects.all()
+    serializer_class = TextSet.objects.all()
+
+
+class RandomBannerView(RetrieveAPIView):
+    queryset = RandomBanner.objects.all()
+    serializer_class = RandomBannerSerializer
+
+
+class RandomTextSetView(RetrieveAPIView):
+    queryset = RandomTextSet
+    serializer_class = RandomTextSetSerializer
--- advertisement/urls.py
+++ advertisement/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, url
-from .api import BannerView, ReelView
+from .api import BannerView, ReelView, TextSetView, RandomTextSetView, RandomBannerView
 
 urlpatterns = patterns('',
     url(r'^reel/(?P<pk>\d+)$', ReelView.as_view(), name='retrieve-reel'),
     url(r'^banner/(?P<pk>\d+)$', BannerView.as_view(), name='retrieve-banner'),
+    url(r'^text-set/(?P<pk>\d+)$', TextSetView.as_view(), name='retrieve-text-set'),
+    url(r'^random-reel/(?P<pk>\d+)$', RandomBannerView.as_view(), name='retrieve-random-reel'),
+    url(r'^random-banner/(?P<pk>\d+)$', RandomTextSetView.as_view(), name='retrieve-random-banner')
 )
