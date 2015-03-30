276cb8a2ba0603671e975aede7a3e123d58f1715
luismasuelli <luismasuelli@hotmail.com>
2015-03-30 11:22:20 -0500

    django media urls para locales

--- repuestazo/urls.py
+++ repuestazo/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from .admin import site
 
 urlpatterns = patterns('',
@@ -7,4 +9,4 @@ urlpatterns = patterns('',
     url(r'^ads/', include('advertisement.urls')),
     url(r'^replacements/', include('stock.urls')),
     url(r'^admin/', include(site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
