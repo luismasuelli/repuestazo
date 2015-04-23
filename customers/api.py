3c86e7215305325e2ed950b2928eb091ca9603a1
luismasuelli <luismasuelli@hotmail.com>
2015-04-22 21:48:34 -0500

    mail dinamico

--- customers/api.py
+++ customers/api.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+from grimoire.django.dynsettings.utils import settings
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from rest_framework.generics import CreateAPIView
